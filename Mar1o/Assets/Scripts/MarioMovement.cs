353f7678e8bd1a4127c06f8120e56345078a37cb
alanjimenez69 <alanjimenez69@gmail.com>
2022-08-22 00:06:04 -0500

    grounded
    added grounded and raycast but as a note

--- Mar1o/Assets/Scripts/MarioMovement.cs
+++ Mar1o/Assets/Scripts/MarioMovement.cs
@@ -4,39 +4,47 @@ using UnityEngine;
 
 public class MarioMovement : MonoBehaviour
 {
-    public float Acceleration = 15.0f;
-    public float Speed = 1.0f;
-    public float JumpForce = 185.0f;
+	public float Acceleration = 15.0f;
+	public float Speed = 1.0f;
+	public float JumpForce = 185.0f;
 
-    private Rigidbody2D Rigidbody2D;
-    private float Horizontal;
-    private float TimeBetweenJumps = 0.1f;
-    private float LastJump;
-    private float Velocity;
+	private Rigidbody2D Rigidbody2D;
+	private float Horizontal;
+	private float TimeBetweenJumps = 0.1f;
+	private float LastJump;
+	private float Velocity;
+	//private bool Grounded;
 
-    void Start()
-    {
-        Rigidbody2D = GetComponent<Rigidbody2D>();
-        
-    }
+	void Start()
+	{
+		Rigidbody2D = GetComponent<Rigidbody2D>();
+		
+	}
 
-    void Update()
-    {
-        Horizontal = Input.GetAxisRaw("Horizontal");
-        if (Horizontal != 0.0f)
-            Velocity = Mathf.Clamp(Velocity + Horizontal * Acceleration * Time.deltaTime, -1.0f, 1.0f);
-        else
-            Velocity -= Velocity * Acceleration * Time.deltaTime;
-        
-        if (Input.GetKey(KeyCode.Space) && LastJump < Time.time - TimeBetweenJumps)
-        {
-            Rigidbody2D.AddForce(Vector2.up * JumpForce);
-            LastJump = Time.time;
-        }
-    }
+	void Update()
+	{
+		Horizontal = Input.GetAxisRaw("Horizontal");
 
-    private void FixedUpdate()
-    {
-        Rigidbody2D.velocity = new Vector2((Mathf.Abs(Velocity) < 0.01f ? 0.0f : Velocity) * Speed, Rigidbody2D.velocity.y);
-    }
+		//Debug.DrawRay(transform.position, Vector3.down * 0.1f, Color.red)
+		//if (Physics2D.Raycast(transform.position, Vector3.down, 0.1f))
+        //{
+			//Grounded = true;
+       //}
+
+		if (Horizontal != 0.0f)
+			Velocity = Mathf.Clamp(Velocity + Horizontal * Acceleration * Time.deltaTime, -1.0f, 1.0f);
+		else
+			Velocity -= Velocity * Acceleration * Time.deltaTime;
+		
+		if (Input.GetKey(KeyCode.Space) && LastJump < Time.time - TimeBetweenJumps)
+		{
+			Rigidbody2D.AddForce(Vector2.up * JumpForce);
+			LastJump = Time.time;
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		Rigidbody2D.velocity = new Vector2((Mathf.Abs(Velocity) < 0.01f ? 0.0f : Velocity) * Speed, Rigidbody2D.velocity.y);
+	}
 }
